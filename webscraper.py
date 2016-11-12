582b3d3d14f834c0dc399cea05a730555f796e73
daltonhahn <hahnd11@gmail.com>
2016-11-12 05:31:18 -0600

    webscraper working and populating .txt files.  Need to change to .sql files

--- webscraper.py
+++ webscraper.py
@@ -3,13 +3,13 @@ import requests
 import regex
 
 import urllib
-"""Automating for the sake of debugging, uncomment when production ready
+
 year = input("What year would you like to make a schedule for? (All four digits of year entered.)\n")
 semester = raw_input("What semester would you like to make a schedule for? (Spring/Summer/Fall)\n")
 
 schedulePull = semester + str(year) + "/schedule.html"
 print (schedulePull)
-"""
+
 schedulePull= "spring2017/schedule.html"
 data = requests.get("https://courses.ksu.edu/" + schedulePull)
 
@@ -23,40 +23,89 @@ for link in soup.find_all('a'):
     if( len(str(link.get("href"))) < 7 and (str(link.get("href")) != "/" and str(link.get("href")) != "None") ):
         urlList.append(str(link.get("href")))
 
-"""Change this for the full list of URLS once you can do it for one
+courseHeadWrite = open('courseNum.txt', 'w')
+courseHeadWrite.write("INSERT INTO courseHeader\n")
+courseHeadWrite.write("VALUES \n")
+
+courseContentWrite = open('courseCont.txt', 'w')
+courseContentWrite.write("INSERT INTO courseContent\n")
+courseContentWrite.write("VALUES \n")
+
 for url in urlList:
     fullURL = "https://courses.ksu.edu/" + semester + str(year) + "/" + url
 
     r = urllib.urlopen(fullURL).read()
     urlSoup = BeautifulSoup(r, "html.parser")
-    courseHeaders = soup.find_all("tbody", {"class": "course-header"})
-    print(urlSoup.getText())
-    """
+    coursesHeader = urlSoup.find_all('tr', class_='course')
 
 
-fullURL = "https://courses.ksu.edu/" + "spring" + "2017" + "/AG"
-r = urllib.urlopen(fullURL).read()
-urlSoup = BeautifulSoup(r, "html.parser")
-coursesHeader = urlSoup.find_all('tr', class_='course')
-coursesContentOdd = urlSoup.find_all('tbody', class_='section')
-coursesContentEven = urlSoup.find_all('tbody', class_='section even')
 
-courseHeadWrite = open('courseNum.txt', 'w')
 
-for c in coursesHeader:
-    for cont in c.contents:
-        modString = (regex.split('(\d+)', cont.get_text()))
-        courseHeadWrite.write(modString[0] + "," + modString[1] + "," + modString[2] + "\n")
+    headerCount = 0
+    for c in coursesHeader:
+        for cont in c.contents:
+            if(headerCount >= len(coursesHeader) - 1):
+                modString = (regex.split('(\d+)', cont.get_text()))
+                courseHeadWrite.write("(\"" + modString[0] + "\"" + "," + "\"" + modString[1] + "\"" + "," + "\"" + modString[2] + "\")" + "\n")
+            else:
+                modString = (regex.split('(\d+)', cont.get_text()))
+                courseHeadWrite.write("(\"" + modString[0] + "\"" + "," + "\"" + modString[1] + "\"" + "," + "\"" + modString[2] + "\")," + "\n")
+                headerCount += 1
+
+    courseHeadWrite.write(";")
+
+
+
+
+
+
+    coursesContent = urlSoup.find_all('tbody', class_='section')
+
+
+
+    contentCount = 0
+    for data in coursesContent:
+        if(contentCount >= len(coursesContent) - 1):
+            for sectionRow in data.find_all('tr', class_='st'):
+                counter = 0
+                courseContentWrite.write("(")
+                for elem in sectionRow.find_all('td'):
+                    if(counter != 4 and counter != 8 and counter != 10):
+                        if(elem.get_text() == "Appointment"):
+                            courseContentWrite.write("\"\", \"\", ")
+                            counter += 1
+                            counter += 1
+                        else:
+                            if(counter == 9):
+                                courseContentWrite.write("\"" + elem.get_text().encode("utf-8") + "\"")
+                            else:
+                                courseContentWrite.write("\"" + elem.get_text().encode("utf-8") + "\"" + ", ")
+                            counter += 1
+                    else:
+                        counter += 1
+                courseContentWrite.write(")")
+                courseContentWrite.write(";")
+        else:
+            for sectionRow in data.find_all('tr', class_='st'):
+                counter = 0
+                courseContentWrite.write("(")
+                for elem in sectionRow.find_all('td'):
+                    if(counter != 4 and counter != 8 and counter != 10):
+                        if(elem.get_text() == "Appointment"):
+                            courseContentWrite.write("\"\", \"\", ")
+                            counter += 1
+                            counter += 1
+                        else:
+                            if(counter == 9):
+                                courseContentWrite.write("\"" + elem.get_text().encode("utf-8") + "\"")
+                            else:
+                                courseContentWrite.write("\"" + elem.get_text().encode("utf-8") + "\"" + ", ")
+                            counter += 1
+                    else:
+                        counter += 1
+                courseContentWrite.write(")")
+                courseContentWrite.write(",\n")
+        contentCount += 1
+
 courseHeadWrite.close()
-
-for d in coursesContentOdd:
-    for cont in d.contents[0]:
-        print(cont.get_text())
-    print
-
-"""
-for d in coursesContentEven:
-    for cont in d.contents:
-        print(cont.get_text())
-    print("\n")
-"""
+courseContentWrite.close()
